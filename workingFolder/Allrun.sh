ff88bb1419d66d967f08798570cfd66904deabf6
Ran Wang <randombazinga@gmail.com>
2018-06-07 15:53:42 -0400

    add batch delete command

--- workingFolder/Allrun.sh
+++ workingFolder/Allrun.sh
@@ -71,5 +71,40 @@
 
 ## streamline visulization from xy plane for 0 30 45 and 60 cases Mon Jan 15 16:21:01 EST 2018                                                                                                   
 
-mkdir -p 0/Time/Process/streamline/streamwiseView
-pvpython ./paraScript/streamline/streamWiseView/streamlineView_0deg.py > log.streamline-0deg.py &
+# mkdir -p 0/Time/Process/streamline/streamwiseView
+# pvpython ./paraScript/streamline/streamWiseView/streamlineView_0deg.py > log.streamline-0deg.py &
+
+
+## Useful commands #1:
+### project structure:
+# .
+# ├── Allrun.sh
+# └── case1
+#  └── 0
+#   └── dummy
+#  └── processor1
+#   └── 0
+#   └── 1
+#   └── 2
+#   └── constant
+
+# the following code will delete the time data in processor1 folder
+# except 0 20 and constant folder
+# It is useful when the case is being reconstructed.
+
+curdir=$(pwd)
+
+    for f in $curdir/*
+
+        do [ -d $f ] &&
+
+            cd "$f" &&
+
+            echo Entering into $f &&
+
+               {
+
+		   find  ./processor*/* -maxdepth 0  ! \( -name 0 -o -name 20 -o -name constant \)  -type d -exec rm -rf {} +
+	       }
+
+       done;
