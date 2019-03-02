6bc65c24dbd878d3c954fedddd28b501c692d408
Ran Wang <randombazinga@gmail.com>
2019-03-02 11:49:19 -0500

    switch case status by copy new file
    better use && at cp line to aviod potential cp name conflict

--- preparingFolder/runningScript/switch2Sampling.sh
+++ preparingFolder/runningScript/switch2Sampling.sh
@@ -1,16 +1,21 @@
 #!bin/bash
 
 curdir=$(pwd)
+basdir="$(dirname "$curdir")"
 
 for f in $curdir/*
 	 
-do [ -d $f -a $f != "$curdir/Re14E4LSDense" -a $f != "$curdir/TSRe14E4CoasraMesh" ] &&
+do [ -d $f -a $f != "$curdir/Allrun" -a $f != "$curdir/Re14E4LSDense" -a $f != "$curdir/TSRe14E4CoasraMesh" ] &&
        
        cd "$f" &&
 
        echo Entering into $f &&
 
-       sleep 1
-   
+       cp -f "$basdir/preparingFolder/prepareCases/switchCase/system/controlDict" ./system &&
+       cp -f "$basdir/preparingFolder/prepareCases/switchCase/system/dummyA" ./system &&
+       cp -f "$basdir/preparingFolder/prepareCases/switchCase/system/dummyB" ./system &&
+       
+       sleep 1 ;
+       
        cd ..
 done;
