6a94e588cc4f88cc5638e32dabce2db13c4ac689
Ran Wang <randombazinga@gmail.com>
2019-05-08 10:59:45 -0400

    write a openfoam-mode?

---
+++ openfoam.el
@@ -0,0 +1,13 @@
+;; add dummy file for paraview visulization
+(eval-after-load 'dired
+  '(progn
+     (define-key dired-mode-map (kbd "C-c n") 'my-test-new)
+     (defun my-test-new ()
+       (interactive)
+       (write-region
+        "for paraview foo" nil
+        (concat (dired-current-directory)
+                (nth (- (length (split-string
+                                 (dired-current-directory) "/")) 2)
+                     (split-string
+                      (dired-current-directory) "/")) ".foam") t))))
