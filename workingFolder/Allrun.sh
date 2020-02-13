e2bb39360083bead502296b066aefc34289ba828
Ran Wang <wang1hf@uwindsor.ca>
2020-02-13 12:05:59 -0500

    yes, of course, it is

--- workingFolder/Allrun.sh
+++ workingFolder/Allrun.sh
@@ -7,7 +7,7 @@
 #                 |
 #------------------------------------------------------------------------------
 # License
-#     This file is NOT part of TEM.
+#     This file is part of TEM.
 #
 #     TEM is free software: you can redistribute it and/or modify it
 #     under the terms of the GNU General Public License as published by
