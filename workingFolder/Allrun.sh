49d4b2ba8e62fb036d52379e2eaafa2721ccbd58
Ran Wang <wang1hf@uwindsor.ca>
2020-02-13 12:05:05 -0500

    add banner

--- workingFolder/Allrun.sh
+++ workingFolder/Allrun.sh
@@ -1,4 +1,39 @@
 #!bin/bash
+#------------------------------------------------------------------------------
+#                 |
+#  This need      | TEM: Talent Elemetn Method
+#                 | Website:  https://github.com/randomwangran/TEM
+# Further refine  | Copyright (C) 2018-2020 randomwangran
+#                 |
+#------------------------------------------------------------------------------
+# License
+#     This file is NOT part of TEM.
+#
+#     TEM is free software: you can redistribute it and/or modify it
+#     under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     TEM is distributed in the hope that it will be useful, but WITHOUT
+#     ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#     FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+#     for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with TEM.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Script
+#     Allrun.sh
+#
+# Description
+#
+#     Just like the role of James T. Kirk
+#
+#     Want to read more of his story, visit:
+#
+#             https://en.wikipedia.org/wiki/James_T._Kirk
+#
+#------------------------------------------------------------------------------
 
 ## if the running require multiple branches to run, make sure the
 ## submission is after ENTERING the proper working directory
