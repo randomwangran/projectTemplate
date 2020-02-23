9ca5a04086a1a006267daddbbc60f9f1d8bbb8fb
Ran Wang <wang1hf@uwindsor.ca>
2020-02-23 18:00:43 -0500

    [todo] need a method

--- preparingFolder/runningScript/submitJobs.sh
+++ preparingFolder/runningScript/submitJobs.sh
@@ -3,14 +3,16 @@
 curdir=$(pwd)
 
 for f in $curdir/*
-do 
+do
        cd "$f" &&
 
        echo Entering into $f &&
 
        echo $ > tmp
        ReynoldsNum=`grep -oP '(?<=Re_)[0-9]+' tmp`
-       
+
+       # make the purpose 1st arg
+       # then var start with 2nd and so or, good for naming issue
        sbatch --job-name=purposeOfJobs_$(ReynoldsNum) --output=roundness4Re_$(ReynoldsNum)_%N_%J.log ./decomposedAndSubmit.pbs pisoFoam &&
 
        rm tmp
