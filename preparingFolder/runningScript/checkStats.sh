fcafce9fcf42d00c9135a049b9397b59ca1bfb64
Ran Wang <wang1hf@uwindsor.ca>
2020-03-29 21:42:24 -0400

    [TCP] detector

--- preparingFolder/runningScript/checkStats.sh
+++ preparingFolder/runningScript/checkStats.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+
+# ORTE can happen at any place, nto a robust way to do this
+
 anchForTimeStepLineNum=$(grep -n "ORTE" $1  | cut -d : -f 1)
 
-preAnch=$(bc <<< " $anchForTimeStepLineNum - 11 ")
+preAnch=$(bc <<< " $anchForTimeStepLineNum - 20 ")
 
 sed -n "$preAnch,$anchForTimeStepLineNum p" $1 | sed -n '/Time = [0-9]/p'
